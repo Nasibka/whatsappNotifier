8f3ac4a0bb0380656c745056c0e00c2686d50084
Nasibka <nasibavalieva.2000@gmail.com>
2021-11-08 20:43:25 +0600

    fisrt commit

--- wpConfig.js
+++ wpConfig.js
@@ -3,25 +3,25 @@ module.exports = {
 
     seller:{
         url:'https://eu191.chat-api.com/instance207181/',
-        token:'?token=g5jjnhmzxzra33ls'
+        token:'?token=******'
     },
     seller2:{
         url:'https://api.chat-api.com/instance297490/',
-        token:'?token=s5nabq54zwm1e38c'
+        token:'?token=******'
     },
     buyer:{
         url:'https://eu90.chat-api.com/instance153453/',
-        token:'?token=lnbab8d8pjffdusc'
+        token:'?token=******'
     },
     verification:{
         url:'https://eu155.chat-api.com/instance154690/',
-        token:'?token=zi2874v38u2sxyrs'
+        token:'?token=******'
     },
     
-    sellerUrl:'https://eu191.chat-api.com/instance207181/message?token=g5jjnhmzxzra33ls',//sellerWP
-    sellerUrl2:'https://api.chat-api.com/instance297490/message?token=s5nabq54zwm1e38c',//sellerWP2
-    buyerUrl:'https://eu90.chat-api.com/instance153453/message?token=lnbab8d8pjffdusc',//auctionWhatsapp
-    verificationUrl:'https://eu155.chat-api.com/instance154690/message?token=zi2874v38u2sxyrs',
+    sellerUrl:'https://eu191.chat-api.com/instance207181/message?token=******',//sellerWP
+    sellerUrl2:'https://api.chat-api.com/instance297490/message?token=******',//sellerWP2
+    buyerUrl:'https://eu90.chat-api.com/instance153453/message?token=******',//auctionWhatsapp
+    verificationUrl:'https://eu155.chat-api.com/instance154690/message?token=******',
     testUrl:''
 
 } 
